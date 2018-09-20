ecc5225caa95c9a52d37fe2ebbdcfec1748eabcf
yoichi kobayashi <info@tplh.net>
2018-09-21 00:24:31 +0900

    created basis files.

---
+++ redux-calculator/src/containers/CalculatorContainer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class CalculatorContainer extends React.Component {
+  render() {
+    return (
+      <div></div>
+    )
+  }
+}
+export default CalculatorContainer;
--- redux-calculator/src/index.js
+++ redux-calculator/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import CalculatorContainer from './containers/CalculatorContainer';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+  <CalculatorContainer />,
+  document.getElementById('root')
+);
