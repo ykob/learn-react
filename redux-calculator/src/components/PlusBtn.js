87571ab4322b6b98896c85f8e0ac89d1efec60c9
yoichi kobayashi <info@tplh.net>
2018-09-21 00:41:58 +0900

    Defined components.

---
+++ redux-calculator/src/components/PlusBtn.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const PlusBtn = ({n}) => (
+  <button>+</button>
+);
+
+export default PlusBtn;
--- redux-calculator/src/containers/CalculatorContainer.js
+++ redux-calculator/src/containers/CalculatorContainer.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import NumBtn from '../components/NumBtn';
+import PlusBtn from '../components/PlusBtn';
+import Result from '../components/Result';
 
 class CalculatorContainer extends React.Component {
   render() {
     return (
-      <div></div>
+      <div>
+        <div>
+          <NumBtn n = {1} />
+          <NumBtn n = {2} />
+          <NumBtn n = {3} />
+        </div>
+        <div>
+          <NumBtn n = {4} />
+          <NumBtn n = {5} />
+          <NumBtn n = {6} />
+        </div>
+        <div>
+          <NumBtn n = {7} />
+          <NumBtn n = {8} />
+          <NumBtn n = {9} />
+        </div>
+        <div>
+          <NumBtn n = {0} />
+          <PlusBtn />
+        </div>
+        <div>
+          <Result />
+        </div>
+      </div>
     )
   }
 }
