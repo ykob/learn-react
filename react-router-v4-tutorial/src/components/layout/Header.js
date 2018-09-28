94de15971bbb9c8ba70281c0773a248ca09e296d
yoichi kobayashi <info@tplh.net>
2018-09-28 13:53:56 +0900

    added Link components in the Header component.

--- react-router-v4-tutorial/src/components/layout/Header.js
+++ react-router-v4-tutorial/src/components/layout/Header.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => (
-  <div>
-    <p>Header</p>
-  </div>
+  <header>
+    <ul>
+      <li><Link to='/'>Home</Link></li>
+      <li><Link to='/page1'>Page1</Link></li>
+      <li><Link to='/page2'>Page2</Link></li>
+      <li><Link to='/page3'>Page3</Link></li>
+    </ul>
+  </header>
 );
 
 export default Header;
